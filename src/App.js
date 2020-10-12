c2d46072aa8c7f4b8baaab4adf06c3cdccdbcb5a
Ikhwan <khusayni@gmail.com>
2020-10-13 02:01:21 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
-  Route
+  Route,
 } from "react-router-dom"
 import Header from './Components/Header'
 import Footer from './Components/Footer'

7416e9c30e9cfd65169ec4ca1973eeb377bec21f
Falahuddin <falahuddinas@gmail.com>
2020-10-13 02:03:49 +0800

    Updates

---
+++ precache-manifest.8bbd3c4f2f1a35cc2a84caafec1cba75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69625a9c774e7fc4dd31b4a2c18d0d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411c857796f12937a6f3",
+    "url": "/static/css/main.18113ed6.chunk.css"
+  },
+  {
+    "revision": "ef86d2ecc31d81de8c70",
+    "url": "/static/js/2.4650eabd.chunk.js"
+  },
+  {
+    "revision": "1676cfeecbcf61be862405d69569c73d",
+    "url": "/static/js/2.4650eabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411c857796f12937a6f3",
+    "url": "/static/js/main.a437161c.chunk.js"
+  },
+  {
+    "revision": "164a07ece64d3a88c2be",
+    "url": "/static/js/runtime-main.aa147ffe.js"
+  },
+  {
+    "revision": "9c695255eab30bf96ae3121cb1f94d16",
+    "url": "/static/media/flag.9c695255.jpg"
+  }
+]);
